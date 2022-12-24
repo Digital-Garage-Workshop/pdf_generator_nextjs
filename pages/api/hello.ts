91c618233477fab0e9a0bc876fc6bf5abbadbe59
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-12-25 00:02:23 +0800

    misc

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,31 +5,15 @@ import puppeteer from "puppeteer-core";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { url } = req.body;
-    res.status(200).send(url);
 
-    // const options = process.env.AWS_REGION
-    //   ? {
-    //       args: chrome.args,
-    //       executablePath: await chrome.executablePath,
-    //       headless: chrome.headless,
-    //     }
-    //   : {
-    //       args: [],
-    //       executablePath:
-    //         process.platform === "win32"
-    //           ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-    //           : process.platform === "linux"
-    //           ? "/usr/bin/google-chrome"
-    //           : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-    //     };
-    // const browser = await puppeteer.launch({
-    //   args: chrome.args,
-    //   executablePath: await chrome.executablePath,
-    //   headless: chrome.headless,
-    // });
-    // const page = await browser.newPage();
-    // await page.goto(url as string, { waitUntil: "networkidle0" });
-    // return await page.screenshot({ type: "png" });
+    const browser = await puppeteer.launch({
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    });
+    const page = await browser.newPage();
+    await page.goto(url as string, { waitUntil: "networkidle0" });
+    return await page.screenshot({ type: "png" });
   } catch (error) {
     console.log(error);
     res.status(400).send(JSON.stringify(error));
