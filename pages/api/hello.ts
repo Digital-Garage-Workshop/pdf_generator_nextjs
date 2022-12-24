5135ae417f7ac11aaa82d4f665c25248f1aabae9
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-12-24 23:45:56 +0800

    misc

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,26 +3,31 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import puppeteer from "puppeteer-core";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { url } = req.query;
+  try {
+    const { url } = req.body;
 
-  const options = process.env.AWS_REGION
-    ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-      }
-    : {
-        args: [],
-        executablePath:
-          process.platform === "win32"
-            ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-            : process.platform === "linux"
-            ? "/usr/bin/google-chrome"
-            : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-      };
-  const browser = await puppeteer.launch(options);
-  const page = await browser.newPage();
-  await page.goto(url as string, { waitUntil: "networkidle0" });
-  return await page.screenshot({ type: "png" });
+    const options = process.env.AWS_REGION
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          args: [],
+          executablePath:
+            process.platform === "win32"
+              ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+              : process.platform === "linux"
+              ? "/usr/bin/google-chrome"
+              : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        };
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+    await page.goto(url as string, { waitUntil: "networkidle0" });
+    return await page.screenshot({ type: "png" });
+  } catch (error) {
+    console.log(error);
+    res.end();
+  }
 };
 export default handler;
